cd99d975cce959e48f2c34c34cac36061baaa1aa
halkaabi3 <halkaabi3@mail.csuchico.edu>
2018-02-12 22:58:19 -0800

    Fortune telling

--- main.cpp
+++ main.cpp
@@ -6,12 +6,14 @@ using namespace std;
 
 int main()
 {
-  //declare variables
+
+//declare variables
 
 string first_name;
 string last_name;
+int lucky;
 
-  //get user input
+//get user input
 
 cout<<"What's your first name"<<endl;
 cin>>first_name;
@@ -19,13 +21,11 @@ cin>>first_name;
 cout<<"What's your last name"<<endl;
 cin>>last_name;
 
-cout<<"Welcome "<<first_name[0]<<"."<<last_name[0]<<"."<<", here is your fortune..."<<endl;
-
-
-
-
 //tell fortune
 
+cout<<"Welcome "<<first_name[0]<<"."<<last_name[0]<<"."<<", here is your fortune..."<<endl;
+
+cout<<"your lucky number is "<<first_name.length()<<endl;
 
 
   return 0;

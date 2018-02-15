67c62b3342faf48714fc59db491f2aec089208bf
halkaabi3 <halkaabi3@mail.csuchico.edu>
2018-02-15 11:22:01 -0800

    Are you lucky ??

--- main.cpp
+++ main.cpp
@@ -27,6 +27,35 @@ cout<<"Welcome "<<first_name[0]<<"."<<last_name[0]<<"."<<", here is your fortune
 
 cout<<"your lucky number is "<<first_name.length()<<endl;
 
+if (first_name[0] == 'U' || first_name[0] == 'u' || first_name[0] == 'A' || first_name[0] == 'a' || first_name[0] == 'E' || first_name[0] == 'e' || first_name[0] == 'I' || first_name[0] == 'i' || first_name[0] == 'O' || first_name[0] == 'o')
+
+{
+cout<<"You are destined to be famous!!"<<endl;
+}
+
+else {
+
+cout<<"You should keep a low profile"<<endl;
+
+}
+
+
+
+
+if ( last_name[last_name.length() - 1] == 'a' || last_name[last_name.length() - 1] == 'A' || last_name[last_name.length() - 1] == 'u' || last_name[last_name.length() - 1] == 'U' || last_name[last_name.length() - 1] == 'e' || last_name[last_name.length() - 1] == 'E' || last_name[last_name.length() - 1] == 'i' || last_name[last_name.length() - 1] == 'I' || last_name[last_name.length() - 1] == 'o' || last_name[last_name.length() - 1] == 'O')
+
+{
+
+
+cout<<"You have already met your true love"<<endl;
+
+
+}
+
+
+else{}
+
+cout<<"Have a good day !"<<endl;
 
   return 0;
 }
